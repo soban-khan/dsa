eba78588f36057e4868966511fa557c29a52c9ad
Soban Khan <soban@Sobans-MacBook-Air.local>
2021-03-16 15:33:24 +0530

    left_rotate_array_of_size_n_by_d_elements_python

---
+++ array/left_rotate_array_of_size_n_by_d_elements.py
@@ -0,0 +1,19 @@
+# Rotate One by One
+def left_rotate_by_one(arr):
+    temp = arr[0]
+    for i in range(len(arr)-1):
+        arr[i] = arr[i+1]
+    arr[len(arr)-1] = temp
+    return arr
+
+
+def left_rotate(arr, d):
+    for i in range(d):
+        left_rotate_by_one(arr)
+    return arr
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 3, 4, 5, 6]
+    print(arr)
+    print(left_rotate(arr, 2))
