b5744c0f496374d5ee10b1c80cd73eaa5f813630
Soban Khan <soban@Sobans-MacBook-Air.local>
2022-01-18 22:01:22 +0530

    Matrix

---
+++ matrix/minOfEachRowAndColumn.js
@@ -0,0 +1,27 @@
+function minInRow(matrix) {
+    let rowMin = {}
+    for (let i = 0; i < matrix.length; i++) {
+        let min = matrix[i][0]
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] < min) min = matrix[i][j]
+        }
+        rowMin[i + 1] = min
+    }
+    return rowMin
+}
+
+function minInColumn(matrix) {
+    let columnMin = {}
+    for (let j = 0; j < matrix[0].length; j++) {
+        let min = matrix[0][j]
+        for (let i = j; i < matrix.length; i++) {
+            if (matrix[i][j] < min) min = matrix[i][j]
+        }
+        columnMin[j + 1] = min
+    }
+    return columnMin
+}
+
+console.log(minInRow([[20, 5, 15], [14, 54, 9], [60, 45, 32]]))
+
+console.log(minInColumn([[20, 5, 15], [14, 54, 9], [60, 45, 32]]))
