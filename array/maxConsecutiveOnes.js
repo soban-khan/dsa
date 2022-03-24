4b2e0ca0637f36db21cf4e2c61b016f701021de4
soban.khan <soban.khan@primathon.in>
2022-03-24 11:48:11 +0530

    contiguous sum

--- array/maxConsecutiveOnes.js
+++ array/maxConsecutiveOnes.js
@@ -3,13 +3,13 @@
 function max_consecutive_ones(nums) {
     let cnt = 0
     let mcnt = 0
-    for (let i=0;i<nums.length;i++){
-        if (nums[i] == 1){cnt++}
-        else {cnt =0}
-        if (cnt > mcnt){mcnt = cnt}
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) { cnt++ }
+        else { cnt = 0 }
+        mcnt = Math.max(cnt, mcnt)
     }
     return mcnt
 };
 
-arr = [1, 1, 1]
+arr = [1, 1, 1, 1]
 console.log(max_consecutive_ones(arr))
--- array/maxContiguousSum.js
+++ array/maxContiguousSum.js
@@ -1,5 +1,9 @@
 // find the max contiguous sum in an array
 
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
 function maxContiguousSum(arr) {
     let max = Number.MIN_VALUE
     let currentMax = 0
