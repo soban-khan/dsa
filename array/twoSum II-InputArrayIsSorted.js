cae75427f34e78f8248ccb3b8461bdc83a8b7775
soban.khan <soban.khan@primathon.in>
2022-03-25 12:18:29 +0530

    two sum two

---
+++ array/twoSum II-InputArrayIsSorted.js
@@ -0,0 +1,28 @@
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+// find two numbers such that they add up to a specific target number. 
+// Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+// Your solution must use only constant extra space.
+
+// Input: numbers = [2,7,11,15], target = 9
+// Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+var twoSum = function (numbers, target) {
+    let start = 0,
+        end = numbers.length - 1
+    while (start < end) {
+        if (numbers[start] + numbers[end] > target) end--
+        else {
+            if (numbers[start] + numbers[end] === target) return [start + 1, end + 1]
+            start++
+        }
+
+    }
+};
+
+
+let arr = [2, 7, 11, 15]
+let target = 9
+console.log(twoSum(arr, target))
