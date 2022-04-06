280b7a87f264a449c554d0f5d929cf9c793b24b7
soban.khan <soban.khan@primathon.in>
2022-04-06 16:41:07 +0530

    selection sort

---
+++ algos/selectionSort.js
@@ -0,0 +1,28 @@
+// The selection sort algorithm sorts an array by repeatedly finding the minimum element 
+// (considering ascending order) from unsorted part and putting it at the beginning. 
+// The algorithm maintains two subarrays in a given array.
+// 1) The subarray which is already sorted. 
+// 2) Remaining subarray which is unsorted.
+// In every iteration of selection sort, the minimum element (considering ascending order) 
+// from the unsorted subarray is picked and moved to the sorted subarray. 
+
+
+function swap(arr, toIndex, fromIndex) {
+    let temp = arr[toIndex]
+    arr[toIndex] = arr[fromIndex]
+    arr[fromIndex] = temp
+}
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) minIndex = j
+        }
+        swap(arr, minIndex, i)
+    }
+    console.log(arr)
+}
+
+let arr = [64, 25, 12, 22, 11];
+selectionSort(arr)
