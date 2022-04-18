2271659b19eddc587f1040a44c67a42e2bcf0b8a
soban-khan <soban.khanf@gmail.com>
2022-04-18 15:30:35 +0530

    insertion sort

--- algos/insertionSort.js
+++ algos/insertionSort.js
@@ -26,15 +26,12 @@ function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         let j = i - 1
         let key = arr[i]
-        // if (arr[i - 1] > arr[i]) {
-        //     j = i - 1
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j]
             j--
         }
         arr[j + 1] = key
     }
-    // }
     return arr
 }
 
